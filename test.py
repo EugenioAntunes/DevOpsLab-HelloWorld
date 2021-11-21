1eab8ac9beb7734fab2a5c2eee91e5883f4d28bd
EugenioAtnunes <eugenio.antunes@yahoo.com.br>
2021-11-20 22:47:06 -0500

    Corrigindo Código

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "Pipeline DevOps - EugenioAntunes")
+        self.assertRegex(result.data.decode(), "Pipeline DevOps - Eugenio Antunes")
 
 
 if __name__ == "__main__":
